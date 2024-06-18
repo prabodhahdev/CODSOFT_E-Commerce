41843ab2130a910592e50f50c04c17bcfb46ed60
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2024-06-18 12:04:44 +0530

    sidebar

--- admin/src/Pages/Admin/Admin.jsx
+++ admin/src/Pages/Admin/Admin.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './Admin.css'
+import SideBar from '../../Components/SideBar/SideBar'
 const Admin = () => {
   return (
     <div className='admin'>
-      
+      <SideBar/>
     </div>
   )
 }
