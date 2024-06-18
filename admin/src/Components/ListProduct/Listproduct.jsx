7b2fcc709567fcfeef8b92b9083aded0a4d6f367
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2024-06-18 15:51:39 +0530

    Add Product Commit

--- admin/src/Components/ListProduct/Listproduct.jsx
+++ admin/src/Components/ListProduct/Listproduct.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import './listproduct.css'
+import './Listproduct.css'
 const Listproduct = () => {
   return (
     <div className='list-product'>
-      
+      <h1>hello</h1>
     </div>
   )
 }
